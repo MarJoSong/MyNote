1a18f8f022b9532aee082733a0453da8a59add28
marjosong <769200142@qq.com>
2021-09-12 15:40:30 +0800

    move r2o to a depandent project

---
+++ mainwindow.cpp
@@ -0,0 +1,131 @@
+﻿#include "mainwindow.h"
+
+#include <iostream>
+
+#include "ui_mainwindow.h"
+
+void MainWindow::replyFinished(QNetworkReply *reply) {
+  if (reply->error() != QNetworkReply::NoError) {
+    //处理中的错误信息
+    qDebug() << "reply error:" << reply->errorString();
+  } else {
+    //请求方式
+    qDebug() << "operation:" << reply->operation();
+    //状态码
+    qDebug()
+        << "status code:"
+        << reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+    qDebug() << "url:" << reply->url();
+    // qDebug()<<"raw header:"<<reply->rawHeaderList();
+
+    //获取响应信息
+    const QByteArray reply_data = reply->readAll();
+    QString str(reply_data);
+    qDebug() << "read all:" << str;
+    m_oneWords->setText(str);
+  }
+  reply->deleteLater();
+}
+
+void MainWindow::ontimeOut() { GetOneWords(); }
+
+void MainWindow::GetOneWords() {
+  QNetworkRequest request;
+
+  QString urlString;
+  QUrl url;
+  urlString.append("https://v1.hitokoto.cn/?c=d&c=h&encode=text");
+  url.setUrl(urlString);
+
+  QSslConfiguration conf = request.sslConfiguration();
+  conf.setPeerVerifyMode(QSslSocket::VerifyNone);
+  conf.setProtocol(QSsl::AnyProtocol);
+  request.setSslConfiguration(conf);
+
+  request.setUrl(url);
+
+  m_pNetAccMgr->get(request);
+}
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+  ui->tabWidget->tabsClosable();
+
+  m_pNetAccMgr = new QNetworkAccessManager(this);
+  connect(m_pNetAccMgr, &QNetworkAccessManager::finished, this,
+          &MainWindow::replyFinished);
+  connect(&m_timer, SIGNAL(timeout()), this, SLOT(ontimeOut()));
+  connect(ui->fileImp, SIGNAL(triggered()), this, SLOT(fileImp_triggered()));
+
+  m_oneWords = new QLabel("想得到的都拥有，得不到的都释怀。", this);
+  // statusBar()->addWidget(m_oneWords); //靠左
+  statusBar()->addPermanentWidget(m_oneWords);  //靠右
+  m_timer.start(10000);
+}
+
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::fileImp_triggered() {
+  QString aFile = QFileDialog::getOpenFileName(this, "选择数据库文件", "",
+                                               "SQLite数据库(*.db *.db3)");
+  if (aFile.isEmpty()) {
+    QMessageBox::warning(this, "提示", "未选择数据库，\n将使用默认数据库",
+                         QMessageBox::Ok, QMessageBox::NoButton);
+
+    formTable = new QFormTable(this);
+  } else {
+    formTable = new QFormTable(this, aFile);
+  }
+
+  formTable->setAttribute(Qt::WA_DeleteOnClose);
+  int cur = ui->tabWidget->addTab(
+      formTable, QString::asprintf("FAQ %d", ui->tabWidget->count()));
+
+  ui->tabWidget->setCurrentIndex(cur);
+  ui->tabWidget->setVisible(true);
+  ui->editModity->setEnabled(true);
+}
+
+void MainWindow::on_editModity_triggered() {
+  if (formTable != nullptr) {
+    formTable->tableView_readwrite();
+    ui->editModity->setEnabled(false);
+    ui->editReadOnly->setEnabled(true);
+    ui->editAdd->setEnabled(true);
+  }
+}
+
+void MainWindow::on_editReadOnly_triggered() {
+  if (formTable != nullptr) {
+    bool ret = formTable->tableView_save();
+    if (ret) {
+      formTable->tableView_readonly();
+      ui->editReadOnly->setEnabled(false);
+      ui->editModity->setEnabled(true);
+      ui->editAdd->setEnabled(false);
+    }
+  }
+}
+
+void MainWindow::on_editAdd_triggered() {
+  formTable->tableView_append();
+  ui->editSave->setEnabled(true);
+  ui->editCancel->setEnabled(true);
+}
+
+void MainWindow::on_editSave_triggered() {
+  bool ret = formTable->tableView_save();
+  if (ret) {
+    ui->editSave->setEnabled(false);
+    ui->editCancel->setEnabled(false);
+  }
+}
+
+void MainWindow::on_editCancel_triggered() {
+  formTable->tableView_cancel();
+  ui->editSave->setEnabled(false);
+  ui->editCancel->setEnabled(false);
+}
+
+void MainWindow::on_actionClose_triggered() { delete formTable; }
---
+++ qformtable.h
@@ -0,0 +1,73 @@
+﻿#ifndef QFORMTABLE_H
+#define QFORMTABLE_H
+
+#include <QDataWidgetMapper>
+#include <QFileDialog>
+#include <QFontDatabase>
+#include <QItemSelectionModel>
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlRecord>
+#include <QSqlTableModel>
+#include <QStandardItemModel>
+#include <QWebChannel>
+
+#include "document.h"
+#include "previewpage.h"
+#include "stdafx.h"
+
+namespace Ui {
+class QFormTable;
+}
+
+class QFormTable : public QMainWindow {
+  Q_OBJECT
+
+ private:
+  bool openDatabase(const QString &aFile);
+  void openTable();
+
+ public:
+  QFormTable(QWidget *parent = nullptr, const QString &dbfile = "D:\\faq.db");
+  ~QFormTable();
+
+  void tableView_readonly();
+  void tableView_readwrite();
+  void tableView_append();
+  bool tableView_save();
+  void tableView_cancel();
+
+ signals:
+
+ private slots:
+
+  void rbAll_clicked();
+  void rbBasic_clicked();
+  void rbAdvance_clicked();
+  void rbSystem_clicked();
+  void rbNetwork_clicked();
+  void rbDataStru_clicked();
+  void rbAlgorithm_clicked();
+  void rbDatabase_clicked();
+  void rbDesignmode_clicked();
+  void rbOS_clicked();
+  void on_tableView_clicked(const QModelIndex &index);
+  void on_tableView_doubleClicked(const QModelIndex &index);
+
+  void on_rbEffective_clicked();
+
+ private:
+  Ui::QFormTable *ui;
+
+  QSqlDatabase DB;
+  QSqlTableModel *tabModel;
+  QDataWidgetMapper *dataMappler;
+  QStandardItemModel *theModel;       //数据模型
+  QItemSelectionModel *theSelection;  // Item选择模型
+  Document m_content;
+  bool isEditable;
+};
+
+#endif  // QFORMTABLE_H
